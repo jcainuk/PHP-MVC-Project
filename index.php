7df2f8d482f0bfc70068770e888b20d4f0a6afd1
Jonathan Cain <jcainuk@gmail.com>
2024-07-17 17:22:44 +0100

    Simplify front controller logic for dynamic routing
    - Refactored index.php to dynamically require controller files based on the controller parameter
    - Streamlined object instantiation and method calling using variable variables

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <?php
 
-
 $action = $_GET["action"];
 $controller = $_GET["controller"];
 
-if ($controller === "products") {
-  require "src/controllers/products.php";
+require "src/controllers/$controller.php";
 
-  $controller_object = new Products;
-} elseif ($controller === "home") {
-  require "src/controllers/home.php";
+$controller_object = new $controller();
 
-  $controller_object = new Home;
-}
-
-if ($action === "index") {
-  $controller_object->index();
-} elseif ($action === 'show') {
-  $controller_object->show();
-}
+$controller_object->$action();
