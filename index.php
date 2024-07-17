f6596a3f541c41d04b46664f8ef991601f5160e6
Jonathan Cain <jcainuk@gmail.com>
2024-07-17 17:15:20 +0100

    Enhance front controller to support multiple controllers
    - Updated index.php to route requests based on controller parameter
    - Added Home controller with index method for rendering home index page
    - Created views/home_index.php for the home page content

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
-require "src/controllers/products.php";
 
-$controller = new Products;
 
 $action = $_GET["action"];
+$controller = $_GET["controller"];
+
+if ($controller === "products") {
+  require "src/controllers/products.php";
+
+  $controller_object = new Products;
+} elseif ($controller === "home") {
+  require "src/controllers/home.php";
+
+  $controller_object = new Home;
+}
 
 if ($action === "index") {
-  $controller->index();
+  $controller_object->index();
 } elseif ($action === 'show') {
-  $controller->show();
+  $controller_object->show();
 }
