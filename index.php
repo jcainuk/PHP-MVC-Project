0f69e30c549ae8ce0fb34a178ca1e6e6982630db
Jonathan Cain <jcainuk@gmail.com>
2024-07-18 19:40:06 +0100

    adding more routes

--- index.php
+++ index.php
@@ -9,8 +9,13 @@ spl_autoload_register(function (string $class_name) {
 $router = new Framework\Router;
 
 /** Note to self: remove /mvc from the file path in production environment */
+$router->add("/mvc/home", ["controller" => "home", "action" => "index"]);
 $router->add("/mvc/home/index", ["controller" => "home", "action" => "index"]);
+
 $router->add("/mvc/products", ["controller" => "products", "action" => "index"]);
+$router->add("/mvc/products/index", ["controller" => "products", "action" => "index"]);
+
+$router->add("/mvc/products/show", ["controller" => "products", "action" => "show"]);
 $router->add("/mvc/", ["controller" => "home", "action" => "index"]);
 
 $params = $router->match($path);
