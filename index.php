d3886dd64f1150fc61753f214155020d3a4ba837
Jonathan Cain <jcainuk@gmail.com>
2024-07-18 16:52:44 +0100

    Implement URL rewriting and dynamic routing for cleaner URLs
    - Added .htaccess file to enable URL rewriting and route all requests to index.php
    - Updated index.php to parse URL segments for dynamic controller and action handling
    - Refactored index.php to extract controller and action from URL segments for improved routing logic

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 
-$action = $_GET["action"];
-$controller = $_GET["controller"];
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$segments = explode("/", $path);
+
+/**
+ * NOTE: if we print_r($segments) here we get the following. 0 is the slash so empty, then 1,2,3 etc.
+ * Array
+ * [0] => 
+ * [1] => mvc
+ * [2] => home
+ * [3] => index
+ */
+
+$action = $segments[3];
+$controller = $segments[2];
 
 require "src/controllers/$controller.php";
 
