18a6fd4cdb791852dad30cb0bf9cd9d302f98c08
Jonathan Cain <jcainuk@gmail.com>
2024-07-18 17:35:30 +0100

    Add Router class with route management and update index.php for dynamic routing
    - Created add method in Router class to register routes with their parameters
    - Updated index.php to instantiate Router and add predefined routes
    - Added route definitions for home and products controllers in index.php

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
 require "src/router.php";
+
 $router = new Router;
+
+$router->add("/home/index", ["controller" => "home", "action" => "index"]);
+$router->add("/products", ["controller" => "products", "action" => "index"]);
+$router->add("/", ["controller" => "home", "action" => "index"]);
 
 $segments = explode("/", $path);
 
--- src/router.php
+++ src/router.php
@@ -2,4 +2,14 @@
 
 class Router
 {
+
+  private array $routes = [];
+
+  public function add(string $path, array $params): void
+  {
+    $this->routes[] = [
+      "path" => $path,
+      "params" => $params
+    ];
+  }
 }
